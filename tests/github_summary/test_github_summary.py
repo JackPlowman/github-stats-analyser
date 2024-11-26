649f785f79ddadf2476445b2634483deb0ffc484
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-26 18:25:04 +0000

    update

--- tests/github_summary/test_github_summary.py
+++ tests/github_summary/test_github_summary.py
@@ -1,16 +1,19 @@
+from pathlib import Path
+
+from bs4 import BeautifulSoup
 from markdown import markdown
-
-from .utils import get_summary_markdown
-
-owner = "JackPlowman"
-repo = "github-stats-analyser"
-run_id = 11996895865
 
 
 def test_github_summary() -> None:
     # Act
-    summary_markdown = get_summary_markdown(owner, repo, run_id)
-    # Assert
-    # table_html = markdown(summary_markdown, extensions=["markdown.extensions.tables"])
-    # print(table_html)
-    # raise NotImplementedError("Test not implemented")
+    with Path("tests/github_summary/GITHUB_ACTION_SUMMARY.md").open() as file:
+        summary_markdown = file.read()
+    table_html = markdown(summary_markdown, extensions=["markdown.extensions.tables"])
+    bs4_html = BeautifulSoup(table_html, "html.parser")
+    headers = bs4_html.find_all("th")
+    # Assert Headers
+    headers_text = [header.text for header in headers]
+    assert headers_text == ["", "repository", "total_files", "total_commits", "commits", "languages"]
+    # Assert row count
+    rows = bs4_html.find_all("tr")
+    assert len(rows) > 3
