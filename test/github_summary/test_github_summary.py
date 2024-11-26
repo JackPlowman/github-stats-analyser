b5e06e9e1566d9e9e30d6858cd98b47abd6b4e73
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-26 17:36:31 +0000

    update

--- test/github_summary/test_github_summary.py
+++ test/github_summary/test_github_summary.py
@@ -1,5 +1,4 @@
-from bs4 import BeautifulSoup
-from requests import get
+from .utils import get_summary_markdown
 
 owner = "JackPlowman"
 repo = "github-stats-analyser"
@@ -7,13 +6,6 @@ run_id = 11996895865
 
 
 def test_github_summary() -> None:
-    # Arrange
-
-    response = get(f"https://api.github.com/repos/{owner}/{repo}/actions/runs/{run_id}/jobs", timeout=10)
-    # assert response.status_code == 200
-    # page = BeautifulSoup(response.text, "html.parser")
-    # print(page)
-    raise Exception("Test not implemented")
     # Act
-
+    markdown = get_summary_markdown(owner, repo, run_id)
     # Assert
