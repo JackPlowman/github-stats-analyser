d2beac695721867884aeba44997ad6e6f8677bc3
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-26 17:05:39 +0000

    update

--- test/github_summary/test_github_summary.py
+++ test/github_summary/test_github_summary.py
@@ -1,9 +1,19 @@
+from bs4 import BeautifulSoup
 from requests import get
+
+owner = "JackPlowman"
+repo = "github-stats-analyser"
+run_id = 11996895865
 
 
 def test_github_summary() -> None:
     # Arrange
-    response = get("https://github.com/JackPlowman/github-stats-analyser/actions/runs/11996895865", timeout=10)
-    assert response.status_code == 200
+
+    response = get(f"https://api.github.com/repos/{owner}/{repo}/actions/runs/{run_id}/jobs", timeout=10)
+    # assert response.status_code == 200
+    # page = BeautifulSoup(response.text, "html.parser")
+    # print(page)
+    raise Exception("Test not implemented")
     # Act
+
     # Assert
