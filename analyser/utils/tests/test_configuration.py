4e37d8a8ec5a9bfdf5ea55621e1516dd6aa6c60f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-16 21:40:30 +0000

    update

--- analyser/utils/tests/test_configuration.py
+++ analyser/utils/tests/test_configuration.py
@@ -24,10 +24,7 @@ def test_configuration__generate_action_summary_table() -> None:
     # Act
     action_summary_table = configuration.generate_action_summary_table()
     # Assert
-    assert (
-        action_summary_table
-        == f"| Key | Repository Owner |\n| --- | ---------------- |\n| Value | {repo_owner} |"
-    )
+    assert action_summary_table == f"| Key | Repository Owner |\n| --- | ---------------- |\n| Value | {repo_owner} |"
     # Clean Up
     del environ["INPUT_REPOSITORY_OWNER"]
     del environ["INPUT_GITHUB_TOKEN"]
